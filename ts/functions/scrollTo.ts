bc2e5f7534d75c08de839a5434f171dddb4288ad
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-05-09 10:43:08 -0300

    Fix scroll types

--- ts/functions/scrollTo.ts
+++ ts/functions/scrollTo.ts
@@ -1,11 +1,11 @@
-function easeInOutQuad(t, b, c, d) {
+function easeInOutQuad(t: number, b: number, c: number, d: number) {
 	t /= d / 2;
 	if (t < 1) return (c / 2) * t * t + b;
 	t--;
 	return (-c / 2) * (t * (t - 2) - 1) + b;
 }
 
-export function scrollTo(element, to = 0, duration = 1000) {
+export function scrollTo(element: HTMLElement, to = 0, duration = 1000) {
 	const start = element.scrollTop;
 	const change = to - start;
 	const increment = 20;
